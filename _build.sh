bbeb8506aae9e38c676c0cf2e5f1bc8f3dafcc72
Matt Shu <matthewshu88@gmail.com>
2022-11-30 15:01:40 -0500

    add description, travis, and build.sh

---
+++ _build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -ev
+
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+tlmgr option repository ctan
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
